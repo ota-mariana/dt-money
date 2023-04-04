9f2e5cb332544db955419399ad65a108116fe0ee
Mariana <nanomarii10@gmail.com>
2023-04-04 11:22:38 -0300

    feat: new transaction form

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import logo from '../../assets/logotipo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={ logo } alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
