bd48c1b2a338d584ebb0f46b659eff4f7981df8b
Mariana <nanomarii10@gmail.com>
2023-04-05 08:29:03 -0300

    feat: transactions context implementation

---
+++ src/contexts/TransactionsContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+  createdAt: string;
+}
+
+interface TransactionsContextType {
+  transactions: Transaction[];
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+export const TransactionsContext = createContext({} as TransactionsContextType);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function fetchTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={ { transactions } }></TransactionsContext.Provider>
+  )
+}
